823e4e2993ab0d49e40a6c5c46ab44058cbfe84d
uvchuk <uvchuk@gmail.com>
2023-05-06 21:03:48 +0200

    change assets output directory path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: assetInfo => {
+					let extType = assetInfo.name.split(".").at(1);
+					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+						extType = "img";
+					}
+					return `[name]-[hash][extname]`;
+				},
+				chunkFileNames: "[name]-[hash].js",
+				entryFileNames: "[name]-[hash].js",
+			},
+		},
+	},
+});
