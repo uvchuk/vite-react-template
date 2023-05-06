8348b04fab4ff5c1b2d984ec449eda2857d85e10
uvchuk <uvchuk@gmail.com>
2023-05-06 22:07:31 +0200

    change deploy method

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				assetFileNames: assetInfo => {
-					let extType = assetInfo.name.split(".").at(1);
-					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-						extType = "img";
-					}
-					return `[name]-[hash][extname]`;
-				},
-				chunkFileNames: "[name]-[hash].js",
-				entryFileNames: "[name]-[hash].js",
-			},
-		},
-	},
+	base: "/vite-react-template/",
+	// build: {
+	// 	rollupOptions: {
+	// 		output: {
+	// 			assetFileNames: assetInfo => {
+	// 				let extType = assetInfo.name.split(".").at(1);
+	// 				if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+	// 					extType = "img";
+	// 				}
+	// 				return `[name]-[hash][extname]`;
+	// 			},
+	// 			chunkFileNames: "[name]-[hash].js",
+	// 			entryFileNames: "[name]-[hash].js",
+	// 		},
+	// 	},
+	// },
 });
